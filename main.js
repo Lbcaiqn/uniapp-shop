ad939ca7a8c717ab3543002e69274b3a2137c88f
ligengxin549 <ligengxin@gmail.com>
2022-08-06 00:05:49 +0800

    Home1

--- main.js
+++ main.js
@@ -19,3 +19,27 @@ export function createApp() {
   }
 }
 // #endif
+//https://applet-base-api-t.itheima.net/docs-uni-shop/3.home.html#_3-1-%E9%85%8D%E7%BD%AE%E7%BD%91%E7%BB%9C%E8%AF%B7%E6%B1%82
+
+//挂载到uni，可以使用拦截器，里面调用微信小程序的api
+import {$http} from '@escook/request-miniprograM'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+//请求拦截器
+$http.beforeRequest = (opt) => {
+  uni.showLoading({
+    title: '加载中...'
+  })
+}
+//响应拦截器
+$http.afterRequest = (res) => {
+  uni.hideLoading()
+}
+uni.$http = $http
+
+uni.$showToast = (title='网络异常',duration=2000,icon='none') => {
+    uni.showToast({
+        title,
+        duration,
+        icon
+    })
+}
--- pages/Home/Home.vue
+++ pages/Home/Home.vue
@@ -1,22 +1,152 @@
 <template>
-    <view>
-        home
+    <view id="Home">
+        <view>
+            <swiper class="swiper" v-if="data?.swiperList" autoplay circular interval="2000">
+                <swiper-item class="swiper-item" v-for="i in data?.swiperList" :key="i.goods_id">
+                    <navigator 
+                    :url="'/packageA/GoodsDetail/GoodsDetail?goods_id=' + i.goods_id"
+                    :open-type="i.open_type"
+                    >
+                        <image :src="i.image_src" mode="widthFix"></image>
+                    </navigator>
+                </swiper-item>
+            </swiper>
+            
+            
+        </view>
+        <view class="navList" v-if="data?.navList">
+            <view 
+            v-for="(i,iIndex) in data?.navList" 
+            :key="iIndex"
+            @click="clickNav(i.name)"
+            >
+            
+                <image :src="i.image_src" mode="widthFix"></image>
+            </view>
+        </view>
+        <view class="floorList" v-if="data?.floorList">
+            <view 
+            class="floorListItem"
+            v-for="(i,iIndex) in data?.floorList"
+            :key="iIndex"
+            >
+                <view class="floorName"><image :src="i?.floor_title?.image_src" mode="widthFix"></image></view>
+                <view class="floorImage">
+                    <view class="floorImageItem">
+                        <navigator :url="i?.product_list[0].url">
+                            <image
+                            :src="i?.product_list[0].image_src" 
+                            mode="widthFix"
+                            >
+                            </image>
+                        </navigator>
+                    </view>
+                    <view class="floorImageItem right">
+                        <navigator :url="j.url" v-show="jIndex != 0" v-for="(j,jIndex) in i?.product_list" :key="kIndex">
+                          
+                              <image
+                              v-if="jIndex != 0" 
+                              :src="j.image_src" 
+                              mode="aspectFile"
+                              >
+                               </image>
+                          </navigator>
+                        
+                    </view>
+                </view>
+            </view>
+        </view>
+        
+        
     </view>
 </template>
 
-<script>
-    export default {
-        data() {
-            return {
-                
-            }
-        },
-        methods: {
+<script setup>
+import {ref,reactive} from 'vue'
+
+let data = reactive({})
+uni.$http.get('/api/public/v1/home/swiperdata').then(res => {
+    if(res.data.meta.status != 200){
+        uni.$showToast()
+        return
+    }
+    else    data.swiperList = res.data.message  
+})
+
+uni.$http.get('/api/public/v1/home/catitems').then(res => {
+    if(res.data.meta.status != 200){
+        uni.$showToast()
+        return
+    }
+    else    data.navList = res.data.message  
+})
+function clickNav(i){
+    console.log(i)
+    if(i == '分类')   uni.switchTab({url: '/pages/Classification/Classification'})
+}
+
+uni.$http.get('/api/public/v1/home/floordata').then(res => {
+    if(res.data.meta.status != 200){
+        uni.$showToast()
+        return
+    }
+    else    data.floorList = res.data.message  
+    data.floorList.forEach((item) => {
+        item.product_list.forEach((i,iIndex,iList) => {
+            iList[iIndex].url = '/packageA/GoodsCart/GoodsCart?' + i.navigator_url.split('?')[1]
+        })
+    })
+    
+})
+</script>
+
+<style scoped lang="less">
+.swiper {
+    width: 100%;
+    .swiper-item {
+        width: 100%;
+    }
+}
+.navList {
+    display: flex;
+    margin: 7vw 0;
+    view {
+        flex: 1;
+        image {
+            width: 100%;
+        }
+    }
+}
+.floorListItem {
+    margin-bottom: 5vw;
+}
+.floorImage {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0 2vw;
+    
+    image {
+        width: 100%;
+        height: 90%;
+    }
+    .floorImageItem {
+        width: 32%;
+    }
+    .right {
+        width: 62%;
+        
+        display: flex;
+        justify-content: space-around;
+        align-content: space-around;
+        align-content: space-around;
+        flex-wrap: wrap;
+        >navigator {
+            width: 45%;
+            height: 25vw;
+            
             
         }
     }
-</script>
-
-<style>
-
+}
 </style>
